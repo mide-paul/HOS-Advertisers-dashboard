874b7bf426dc4a17ece496fbc27d6555b604289b
Olumide <olumidebamgboje@gmail.com>
2025-01-10 18:53:49 +0100

    updated next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
